21ae49c7e6efe6bc334698d0721724c0b3c8463c
Nevto <Jonathannorberg95@gmail.com>
2024-09-05 18:59:23 +0200

    Fixed the configuration of hardhat, in preparation of deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@typechain/hardhat";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY as string;
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY as string;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY as string;
+
+if (!ALCHEMY_API_KEY || !SEPOLIA_PRIVATE_KEY || !ETHERSCAN_API_KEY) {
+  throw new Error("Missing required environment variables.");
+}
 
 const config: HardhatUserConfig = {
   solidity: "0.8.26",
@@ -8,6 +19,17 @@ const config: HardhatUserConfig = {
     outDir: "typechain-types",
     target: "ethers-v6",
   },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
+
+
